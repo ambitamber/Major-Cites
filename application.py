c37563638b5b9cb5d73f72e9a0c29b42805f9f66
Ambi (Amarjit) Singh <50850921+ambitamber@users.noreply.github.com>
2020-11-10 20:56:43 -0500

    update to Google Place
    Using Google Place to download the image links.

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
-import re
 from bs4 import BeautifulSoup
 import requests
+import json
+from image_application import get_image
 
-city = 'Tours'
-state = 'tx'
-country = 'France'
+city = 'london'
+state = 'ma'
+country = 'United Kingdom'
 
 def get_data_from_web(city,state_or_country):
 
@@ -13,25 +14,27 @@ def get_data_from_web(city,state_or_country):
     soup = BeautifulSoup(data.text, 'html.parser')
     leftcolumn = soup.find('div', class_='leftcolumn')
     related = leftcolumn.find_all('ul', class_='related')
+    #check if related is em not
+    if related:
+        city_list = []
+        for i in related:
+            for city in i.find_all('li'):
+                item = city.text
+                city_list.append(item[item.find('to')+3:])
 
-    city_list = []
-    for i in related:
-        for city in i.find_all('li'):
-            item = city.text
-            city_list.append(item[item.find('to')+3:])
+        city_list.pop(0)
+        del city_list[9:]
 
-    city_list.pop(0)
-    del city_list[9:]
+        return city_list
+    else:
+        print('There is no data for this city.')
 
-    return city_list
-
-
-def get_coutry_data(city_list,country):
+def get_country_data(city_list,country):
     for i in range(len(city_list) - 1, -1, -1):
         if country not in city_list[i]:
+            print('Removing: ' + city_list[i])
             del city_list[i]
-        print('get_coutry_data: ' + str(city_list))
-        return city_list
+    return city_list
 
 def get_US_data(city_list):
     for i in range(len(city_list) - 1, -1, -1):
@@ -40,13 +43,29 @@ def get_US_data(city_list):
     return city_list
 
 def main():
-    if country == "United States":
-        city_list = get_data_from_web(city,state)
-        data = get_US_data(city_list)
-    else:
-        city_list = get_data_from_web(city,country)
-        data = get_coutry_data(city_list,country)
-    print(data)
-
+    print('Getting data for following location: City = ' + city + "State =" + state + "Country =" + country )
+    try:
+        if country == "United States":
+            city_list = get_data_from_web(city,state)
+            data = get_US_data(city_list)
+        else:
+            city_list = get_data_from_web(city,country)
+            data = get_country_data(city_list,country)
+        
+        print("Cities: " + str(data))
+        json_string = {'result':[]}
+        city_id = 1
+        for i in data:
+            image = get_image(i[:i.find(', ')],i[i.find(', ')+2:]).download_image()
+            json_string['result'].append({'id':city_id,'city':i,'image':image})
+            city_id += 1
+        
+        output = json.dumps(json_string)
+    except:
+        output = 'No Data'
+        print('Error getting data.')
+    
+    print(output)
+        
 if __name__ == '__main__':
     main()
---
+++ image_application.py
@@ -0,0 +1,43 @@
+import requests
+import urllib.parse
+import random
+
+class get_image:
+
+    def __init__(self,p1,p2):
+        self.p1 = p1
+        self.p2 = p2
+        self.key = 'AIzaSyB_i8TNJ4TwRYtZMrBXoXNU6iHkXlJvFvQ'
+
+    def download_image(self):
+        query = self.p1 + "+" + self.p2 
+        fields = 'photos'
+        inputtype = 'textquery'
+        main_url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?'
+
+        getparams = {'input':query, 'inputtype':inputtype, 'fields':fields,'key':self.key}
+        url = main_url + urllib.parse.urlencode(getparams)
+        print(url)
+        response = requests.get(url)
+        jsondata = response.json()
+
+        image_id_list = []
+        for photos in jsondata['candidates']:
+            for photo in photos['photos']:
+                image_id_list.append(photo['photo_reference'])
+        
+        if len(image_id_list) == 1:
+            image_url = self.build_image_link(image_id_list[0])
+            return image_url
+        elif len(image_id_list) > 1:
+            image_url = self.build_image_link(random.choice(image_id_list))
+            return image_url
+    
+    def build_image_link(self,image_id):
+        image_url = 'https://maps.googleapis.com/maps/api/place/photo?'
+        maxwidth = '400'
+
+        getparams = {'maxwidth': maxwidth, 'photoreference':image_id, 'key':self.key}
+        imageurl = image_url +  urllib.parse.urlencode(getparams)
+
+        return imageurl
