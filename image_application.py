54297116dc0a8533b91c3e748f8e63ffbc231cba
ambi <ambi92@outlook.com>
2020-12-03 17:39:23 -0500

    added Cloud Firestore

--- image_application.py
+++ image_application.py
@@ -2,6 +2,10 @@ import requests
 import urllib.parse
 import random
 import yaml
+import firebase_admin
+from firebase_admin import credentials,firestore
+import json
+
 
 class get_image:
 
@@ -12,7 +16,7 @@ class get_image:
         configuration = open("configuration.yaml", "r")
         self.configuration = yaml.safe_load(configuration)
     
-    def download_image(self):
+    def Get_Image_Link(self):
         query = self.p1 + "+" + self.p2 
         fields = 'photos'
         inputtype = 'textquery'
@@ -34,7 +38,7 @@ class get_image:
             image_url = self.build_image_link(random.choice(image_id_list))
             return image_url
     
-    def build_image_link(self,image_id):
+    def Build_Image_Link(self,image_id):
         image_url = self.configuration['googleapis']['photo_url']
         maxwidth = '400'
 
@@ -42,3 +46,36 @@ class get_image:
         imageurl = image_url +  urllib.parse.urlencode(getparams)
 
         return imageurl
+
+
+    def Save_to_Firestore(self,cityname,image_url):
+        cred = credentials.Certificate('path/to/serviceAccount.json')
+        firebase_admin.initialize_app(cred)
+
+        db = firestore.client()
+        doc_ref = db.collection(u'users').document(u'alovelace')
+        doc_ref.set({
+            u'first': u'Ada',
+            u'last': u'Lovelace',
+            u'born': 1815
+        })
+
+    def _Check(self):
+        listnum = []
+        for char in self.p1+self.p2.lower():
+            listnum.append(str(ord(char)))
+        
+        db = self.Firestore_Connection()
+        keyID = self.p1 + self.p2 + ":" + '-'.join(listnum)
+        if db.collection('Google City Image').document(keyID).get().exists:
+            return
+    
+    def Firestore_Connection(self):
+        filekey = open("key.json",'rb')
+        key = json.load(filekey)
+        if not firebase_admin._apps:
+            cred = credentials.Certificate(key)
+            firebase_admin.initialize_app(cred)
+        db = firestore.client()
+        return db
+
